c2abf4d6b698661eaa9b8a41f533f70d9f8a01fc
Nazmul Hashan <122245955+nhjeweli@users.noreply.github.com>
2024-01-11 23:27:48 +0600

    Add files via upload

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,14 +1,14 @@
-"use server"
+'use server'
 
-import { revalidatePath } from "next/cache"
+import { revalidatePath } from 'next/cache'
 
-import { connectToDatabase } from "../mongodb/database"
-import User from "../mongodb/database/models/user.model"
-import Order from "../mongodb/database/models/order.model"
-import Event from "../mongodb/database/models/event.model"
-import { handleError } from "../utils"
+import { connectToDatabase } from '@/lib/database'
+import User from '@/lib/database/models/user.model'
+import Order from '@/lib/database/models/order.model'
+import Event from '@/lib/database/models/event.model'
+import { handleError } from '@/lib/utils'
 
-import { CreateUserParams, UpdateUserParams } from "@/types"
+import { CreateUserParams, UpdateUserParams } from '@/types'
 
 export async function createUser(user: CreateUserParams) {
   try {
@@ -27,7 +27,7 @@ export async function getUserById(userId: string) {
 
     const user = await User.findById(userId)
 
-    if (!user) throw new Error("User not found")
+    if (!user) throw new Error('User not found')
     return JSON.parse(JSON.stringify(user))
   } catch (error) {
     handleError(error)
@@ -40,7 +40,7 @@ export async function updateUser(clerkId: string, user: UpdateUserParams) {
 
     const updatedUser = await User.findOneAndUpdate({ clerkId }, user, { new: true })
 
-    if (!updatedUser) throw new Error("User update failed")
+    if (!updatedUser) throw new Error('User update failed')
     return JSON.parse(JSON.stringify(updatedUser))
   } catch (error) {
     handleError(error)
@@ -55,21 +55,24 @@ export async function deleteUser(clerkId: string) {
     const userToDelete = await User.findOne({ clerkId })
 
     if (!userToDelete) {
-      throw new Error("User not found")
+      throw new Error('User not found')
     }
 
     // Unlink relationships
     await Promise.all([
       // Update the 'events' collection to remove references to the user
-      Event.updateMany({ _id: { $in: userToDelete.events } }, { $pull: { organizer: userToDelete._id } }),
+      Event.updateMany(
+        { _id: { $in: userToDelete.events } },
+        { $pull: { organizer: userToDelete._id } }
+      ),
 
       // Update the 'orders' collection to remove references to the user
-      Order.updateMany({ _id: { $in: userToDelete.orders } }, { $unset: { buyer: 1 } })
+      Order.updateMany({ _id: { $in: userToDelete.orders } }, { $unset: { buyer: 1 } }),
     ])
 
     // Delete user
     const deletedUser = await User.findByIdAndDelete(userToDelete._id)
-    revalidatePath("/")
+    revalidatePath('/')
 
     return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null
   } catch (error) {
