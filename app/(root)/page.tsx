fe6809d131e296696bd9a74068eff2e54baa943a
nhjewel <nhjewel75@yahoo.com>
2024-01-13 22:53:58 +0600

    finish the app

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,21 @@
+import CategoryFilter from "@/components/shared/CategoryFilter"
 import Collection from "@/components/shared/Collection"
+import Search from "@/components/shared/Search"
 import { Button } from "@/components/ui/button"
 import { getAllEvents } from "@/lib/actions/event.actions"
+import { SearchParamProps } from "@/types"
 import Image from "next/image"
 import Link from "next/link"
 
-export default async function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1
+  const searchText = (searchParams?.query as string) || ""
+  const category = (searchParams?.category as string) || ""
+
   const events = await getAllEvents({
-    query: "",
-    category: "",
-    page: 1,
+    query: searchText,
+    category,
+    page,
     limit: 6
   })
 
@@ -23,6 +30,7 @@ export default async function Home() {
               <Link href="#events">Explore Now</Link>
             </Button>
           </div>
+
           <Image src="/assets/images/hero.png" alt="hero" width={1000} height={1000} className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]" />
         </div>
       </section>
@@ -31,9 +39,13 @@ export default async function Home() {
         <h2 className="h2-bold">
           Trust by <br /> Thousands of Events
         </h2>
-        <div className="flex w-full flex-col gap-5 md:flex-row">Search CategoryFilter</div>
 
-        <Collection data={events?.data} emptyTitle="No Events Found" emptyStateSubtext="Come back later" collectionType="All_Events" limit={6} page={1} totalPages={2} />
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          <Search />
+          <CategoryFilter />
+        </div>
+
+        <Collection data={events?.data} emptyTitle="No Events Found" emptyStateSubtext="Come back later" collectionType="All_Events" limit={6} page={page} totalPages={events?.totalPages} />
       </section>
     </>
   )
